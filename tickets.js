151d049aa73e041b16693baf09c86f5212035729
jaemarrr <omarmcpaso@gmail.com>
2026-09-24 22:47:37 +0800

    Add 50% VIP surcharge for premium seating - macapaso.omar

--- tickets.js
+++ tickets.js
@@ -2,8 +2,14 @@ function isValidQuantity(quantity) {
   return quantity > 0 && quantity <= 20;
 }
 
-function calculateTicketPrice(quantity, basePrice) {
-  return Math.floor(quantity * basePrice);
+function calculateTicketPrice(quantity, basePrice, seating = 'standard') {
+  let price = quantity * basePrice;
+
+  if (seating === 'premium') {
+    price *= 1.50;
+  }
+
+  return Math.floor(price);
 }
 
 module.exports = { isValidQuantity, calculateTicketPrice };
